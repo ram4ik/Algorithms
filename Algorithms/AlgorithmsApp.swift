1009e7ea352758b59d75c89cfbb9d7f6a70bb6f0
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-31 21:33:07 +0200

    Initial Commit

---
+++ Algorithms/AlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsApp.swift
+//  Algorithms
+//
+//  Created by Ramill Ibragimov on 31.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
